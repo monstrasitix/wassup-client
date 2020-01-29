2b33aeaadd5d4c4245b108225d125feb7a573803
dmitrijssavostijanovs <dmitrijs@groupmail.io>
2020-01-29 22:50:40 +0000

    Patched up fields

--- src/app/views/Landing/Landing.view.tsx
+++ src/app/views/Landing/Landing.view.tsx
@@ -7,42 +7,61 @@ export interface ILandingProps {};
 
 const Buttons = () => (
     <div>
-        <p>Plain</p>
+        <div className="flex">
+            <div>
+                <p>Plain</p>
 
-        <button children="Button" className="button" />
-        <button children="Button" className="button primary" />
-        <button children="Button" className="button secondary" />
+                <button children="Button" className="button" />
+                <button children="Button" className="button primary" />
+                <button children="Button" className="button secondary" />
+            </div>
 
-        <p>Disabled</p>
+            <div>
+                <p>Disabled</p>
 
-        <button children="Button" disabled className="button" />
-        <button children="Button" disabled className="button primary" />
-        <button children="Button" disabled className="button secondary" />
+                <button children="Button" disabled className="button" />
+                <button children="Button" disabled className="button primary" />
+                <button children="Button" disabled className="button secondary" />
+            </div>
+        </div>
+        
 
-        <p>Hollow</p>
+        <div className="flex">
+            <div>
+                <p>Hollow</p>
 
-        <button children="Button" className="button hollow" />
-        <button children="Button" className="button hollow-primary" />
-        <button children="Button" className="button hollow-secondary" />
+                <button children="Button" className="button hollow" />
+                <button children="Button" className="button hollow-primary" />
+                <button children="Button" className="button hollow-secondary" />
+            </div>
 
-        <p>Hollow Disabled</p>
+            <div>
+                <p>Hollow Disabled</p>
 
-        <button children="Button" disabled className="button hollow" />
-        <button children="Button" disabled className="button hollow-primary" />
-        <button children="Button" disabled className="button hollow-secondary" />
+                <button children="Button" disabled className="button hollow" />
+                <button children="Button" disabled className="button hollow-primary" />
+                <button children="Button" disabled className="button hollow-secondary" />
+            </div>
+        </div>
 
-        <p>Pills</p>
+        <div className="flex">
+            <div>
+                <p>Pills</p>
 
-        <button children="Button" className="button pill" />
-        <button children="Button" className="button pill primary" />
-        <button children="Button" className="button pill secondary" />
+                <button children="Button" className="button pill" />
+                <button children="Button" className="button pill primary" />
+                <button children="Button" className="button pill secondary" />
+            </div>
 
-        <br/>
+            <div>
+                <p>Hollow pills</p>
 
-        <button children="Button" className="button pill hollow" />
-        <button children="Button" className="button pill hollow-primary" />
-        <button children="Button" className="button pill hollow-secondary" />
-
+                <button children="Button" className="button pill hollow" />
+                <button children="Button" className="button pill hollow-primary" />
+                <button children="Button" className="button pill hollow-secondary" />
+            </div>
+        </div>
+        
         <p>Sizes</p>
 
         <button children="Button" className="button tiny" />
@@ -51,7 +70,7 @@ const Buttons = () => (
         <button children="Button" className="button large" />
         <button children="Button" className="button massive" />
 
-        <br/>
+        <p>Wide</p>
 
         <button children="Button" className="button wide tiny" />
         <button children="Button" className="button wide small" />
@@ -64,41 +83,129 @@ const Buttons = () => (
 
 const Fields = () => (
     <div>
-        <div className="field">
-            <label className="field-label">Label</label>
-            <input type="text" className="field-input"/>
-            <span className="field-message">Some message</span>
+        <div className="flex">
+            <div className="field">
+                <label className="field-label">Label</label>
+                <span className="field-info">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloremque amet in corrupti temporibus culpa reiciendis itaque
+                    facere optio maiores a placeat, eum veniam voluptates quia,
+                    voluptatum enim quaerat autem suscipit?
+                </span>
+                <input type="text" className="field-input" placeholder="Enter input" />
+                <span className="field-message">Some message</span>
+            </div>
+
+            <div className="field">
+                <label className="field-label">Label</label>
+                <span className="field-info">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloremque amet in corrupti temporibus culpa reiciendis itaque
+                    facere optio maiores a placeat, eum veniam voluptates quia,
+                    voluptatum enim quaerat autem suscipit?
+                </span>
+                <input type="text" disabled className="field-input" placeholder="Enter input" />
+                <span className="field-message">Some message</span>
+            </div>
+        </div>
+
+        <div className="flex">
+            <div className="field success">
+                <label className="field-label">Label</label>
+                <span className="field-info">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloremque amet in corrupti temporibus culpa reiciendis itaque
+                    facere optio maiores a placeat, eum veniam voluptates quia,
+                    voluptatum enim quaerat autem suscipit?
+                </span>
+                <input type="text" className="field-input" placeholder="Enter input" />
+                <span className="field-message">Some message</span>
+            </div>
+
+            <div className="field success">
+                <label className="field-label">Label</label>
+                <span className="field-info">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloremque amet in corrupti temporibus culpa reiciendis itaque
+                    facere optio maiores a placeat, eum veniam voluptates quia,
+                    voluptatum enim quaerat autem suscipit?
+                </span>
+                <input type="text" disabled className="field-input" placeholder="Enter input" />
+                <span className="field-message">Some message</span>
+            </div>
+        </div>
+
+        <div className="flex">
+            <div className="field warning">
+                <label className="field-label">Label</label>
+                <span className="field-info">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloremque amet in corrupti temporibus culpa reiciendis itaque
+                    facere optio maiores a placeat, eum veniam voluptates quia,
+                    voluptatum enim quaerat autem suscipit?
+                </span>
+                <input type="text" className="field-input" placeholder="Enter input" />
+                <span className="field-message">Some message</span>
+            </div>
+
+            <div className="field warning">
+                <label className="field-label">Label</label>
+                <span className="field-info">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloremque amet in corrupti temporibus culpa reiciendis itaque
+                    facere optio maiores a placeat, eum veniam voluptates quia,
+                    voluptatum enim quaerat autem suscipit?
+                </span>
+                <input type="text" disabled className="field-input" placeholder="Enter input" />
+                <span className="field-message">Some message</span>
+            </div>
+        </div>
+
+        <div className="flex">
+            <div className="field error">
+                <label className="field-label">Label</label>
+                <span className="field-info">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloremque amet in corrupti temporibus culpa reiciendis itaque
+                    facere optio maiores a placeat, eum veniam voluptates quia,
+                    voluptatum enim quaerat autem suscipit?
+                </span>
+                <input type="text" className="field-input" placeholder="Enter input" />
+                <span className="field-message">Some message</span>
+            </div>
+
+            <div className="field error">
+                <label className="field-label">Label</label>
+                <span className="field-info">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloremque amet in corrupti temporibus culpa reiciendis itaque
+                    facere optio maiores a placeat, eum veniam voluptates quia,
+                    voluptatum enim quaerat autem suscipit?
+                </span>
+                <input type="text" disabled className="field-input" placeholder="Enter input" />
+                <span className="field-message">Some message</span>
+            </div>
         </div>
 
         <div className="field">
             <label className="field-label">Label</label>
-            <span className="field-message">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Doloremque amet in corrupti temporibus culpa reiciendis itaque
-                facere optio maiores a placeat, eum veniam voluptates quia,
-                voluptatum enim quaerat autem suscipit?
-            </span>
-            <input type="text" className="field-input"/>
-            <span className="field-message">Some message</span>
+            <select className="field-input">
+                <option value="1">Value</option>
+                <option value="1">Value</option>
+                <option value="1">Value</option>
+                <option value="1">Value</option>
+            </select>
         </div>
 
-        <div className="field field--success">
+        <div className="field">
             <label className="field-label">Label</label>
-            <input type="text" className="field-input"/>
-            <span className="field-message">Some message</span>
+            <textarea className="field-input" placeholder="Enter message ..." />
         </div>
 
-        <div className="field field--warning">
-            <label className="field-label">Label</label>
-            <input type="text" className="field-input"/>
-            <span className="field-message">Some message</span>
-        </div>
-
-        <div className="field field--error">
-            <label className="field-label">Label</label>
-            <input type="text" className="field-input"/>
-            <span className="field-message">Some message</span>
-        </div>
+        
+        <p>Checkboxes</p>
+        <p>Radios</p>
+        <p>Switches</p>
     </div>
 );
 
@@ -108,6 +215,13 @@ const Landing: React.FC<ILandingProps> = () => (
         <style children={`
             .button {
                 margin: 5px;
+            }
+            .flex {
+                display: flex;
+            }
+            .flex > * {
+                width: 100%;
+                margin: 1em;
             }
             .a {
                 width: 90%;
